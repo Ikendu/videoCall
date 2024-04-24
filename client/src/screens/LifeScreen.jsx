63c329091a31d452b05546144176c6d20a02442b
Aniede Chibundu <ndubest56@gmail.com>
2024-04-24 07:37:11 +0100

    room form

---
+++ client/src/screens/LifeScreen.jsx
@@ -0,0 +1,17 @@
+function LifeScreen() {
+  return (
+    <div>
+      <h1>LifeScreen</h1>
+      <form>
+        <label htmlFor="email">Email ID</label>
+        <input id="email" type="email" />
+        <br />
+        <label htmlFor="room">room ID</label>
+        <input id="room" type="text" />
+        <br />
+        <button>Join</button>
+      </form>
+    </div>
+  );
+}
+export default LifeScreen;
