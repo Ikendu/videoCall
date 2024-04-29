fe5e3c6d9396fd7bf48f9fd7ddf37cf715205c50
Aniede Chibundu <ndubest56@gmail.com>
2024-04-30 00:44:26 +0100

    video servers offer

--- client/src/services/peer.js
+++ client/src/services/peer.js
@@ -13,4 +13,13 @@ class PeerService {
       });
     }
   }
+  async getOffer() {
+    if (this.peer) {
+        const offer = await this.peer.createOffer();
+        await this.peer.setLocalDescription(new RTCSessionDescription(offer))
+        return offer
+    }
+  }
 }
+
+export default new PeerService()
