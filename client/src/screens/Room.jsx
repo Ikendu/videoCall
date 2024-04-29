d622c7c93c329f6aaf1e6e97ecf0cb9021f3176a
Aniede Chibundu <ndubest56@gmail.com>
2024-04-29 07:59:40 +0100

    more update

--- client/src/screens/Room.jsx
+++ client/src/screens/Room.jsx
@@ -1,18 +1,21 @@
-import { useCallback, useEffect } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { useSocket } from "../context/SockectProvider";
 
 function Room() {
-  const socket = useSocket;
+  const socket = useSocket();
+  const [remoteSocketId, setRemoteSocketId] = useState(null);
 
   const handleUserJoinRoom = useCallback(({ email, id }) => {
-    console.log(`${email} joined the room`);
+      console.log(`${email} joined the room`);
+      setRemoteSocketId(id)
   }, []);
 
   useEffect(() => {
     socket.on("user:joined", handleUserJoinRoom);
+
     return () => socket.off("user:joined", handleUserJoinRoom);
   }, [socket, handleUserJoinRoom]);
 
-  return <div>My Room</div>;
+    return <div>{ remoteSocketId? 'Connected with a user' : 'No one in the room'}</div>;
 }
 export default Room;
