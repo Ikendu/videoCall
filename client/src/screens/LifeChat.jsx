147f4a6db3b4321c8db38a4df35a32ae3115dd66
Aniede Chibundu <ndubest56@gmail.com>
2024-05-03 21:34:52 +0100

    user input

--- client/src/screens/LifeChat.jsx
+++ client/src/screens/LifeChat.jsx
@@ -1,12 +1,33 @@
+import { useCallback, useState } from "react";
+
 function LifeChat() {
+  const [email, setEmail] = useState("");
+  const [room, setRoom] = useState("");
+
+  const handleSubmit = useCallback((e) => {
+    e.preventDefault();
+    console.log({ email, room });
+  }, []);
+
   return (
     <div>
-      <form>
-        <label htmlFor="email"> Email:</label>{" "}
-        <input type="email" id="email" placeholder="exampl@email.com" />
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email"> Email:</label>
+        <input
+          type="email"
+          id="email"
+          placeholder="exampl@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <br />
         <label htmlFor="id">Room ID:</label>
-        <input type="text" id="id" />
+        <input
+          type="text"
+          id="id"
+          value={room}
+          onChange={(e) => setRoom(e.target.value)}
+        />
         <br />
         <button>Join</button>
       </form>
